9f8d279dd4848854a57aec1d6deeec77af979b80
Ignacio Damián González <igonzalez@startuniversity.net>
2023-05-01 15:15:29 -0300

    formulario uploading

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,28 @@
+'use client';
+import { useState } from 'react';
+
 function HomePage() {
-  return <div>HomePage</div>;
+  const [file, setFile] = useState(null);
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!file) return;
+          console.log('Uploading file...');
+        }}
+      >
+        <label>Upload file:</label>
+        <input
+          type='file'
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+          }}
+        />
+        <button>Upload</button>
+      </form>
+    </div>
+  );
 }
 
 export default HomePage;
